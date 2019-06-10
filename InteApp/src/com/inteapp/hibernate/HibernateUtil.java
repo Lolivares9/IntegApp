fbaf7bf2ed5ee5d4f0d9e55b43d5437d695cbe90
matu_ <matu_@DESKTOP-AQ50H9J>
2019-06-10 12:25:14 -0300

    Creada la conexion con la base de datos

--- InteApp/src/com/inteapp/hibernate/HibernateUtil.java
+++ InteApp/src/com/inteapp/hibernate/HibernateUtil.java
@@ -1,5 +1,27 @@
 package com.inteapp.hibernate;
 
-public class HibernateUtil {
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
 
+public class HibernateUtil {
+	private static final SessionFactory sessionFactory;
+	
+    static
+    {
+        try
+        {
+        	AnnotationConfiguration config = new AnnotationConfiguration();
+            sessionFactory = config.buildSessionFactory();
+        }
+        catch (Throwable ex)
+        {
+            System.out.println(ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+ 
+    public static SessionFactory getSessionFactory()
+    {
+        return sessionFactory;
+    }
 }
