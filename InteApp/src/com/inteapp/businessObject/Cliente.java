b5f9fb9f2a0b9994b24984ffe8fd7e9e9dbd8cd5
a305519 <a305519@NBR660925.rio.ar.bsch>
2019-06-10 11:27:11 -0300

    Se agregar clases de negocio

--- InteApp/src/com/inteapp/businessObject/Cliente.java
+++ InteApp/src/com/inteapp/businessObject/Cliente.java
@@ -1,5 +1,92 @@
 package com.inteapp.businessObject;
 
-public class Cliente {
+import java.util.List;
 
+public class Cliente {
+	
+	private String razonSocial;
+	private String cuit;
+	private String mail;
+	private String direccion;
+	private String telefono;
+	private String nroCuenta;
+	private String pais;
+	private String tipoPersona;
+	private List<Empleado> empleados;
+	
+	
+
+	public Cliente(String razonSocial, String cuit, String mail, String direccion, String telefono, String nroCuenta,String pais, String tipoPersona, List<Empleado> empleados) {
+		super();
+		this.razonSocial = razonSocial;
+		this.cuit = cuit;
+		this.mail = mail;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.nroCuenta = nroCuenta;
+		this.pais = pais;
+		this.tipoPersona = tipoPersona;
+		this.empleados = empleados;
+	}
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+	public String getCuit() {
+		return cuit;
+	}
+	public void setCuit(String cuit) {
+		this.cuit = cuit;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getCuenta() {
+		return nroCuenta;
+	}
+	public void setCuenta(String cuenta) {
+		this.nroCuenta = cuenta;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public List<Empleado> getEmpleados() {
+		return empleados;
+	}
+	public void setEmpleados(List<Empleado> empleados) {
+		this.empleados = empleados;
+	}
+	public String getNroCuenta() {
+		return nroCuenta;
+	}
+	public void setNroCuenta(String nroCuenta) {
+		this.nroCuenta = nroCuenta;
+	}
+	public String getTipoPersona() {
+		return tipoPersona;
+	}
+	public void setTipoPersona(String tipoPersona) {
+		this.tipoPersona = tipoPersona;
+	}
+	
 }
---
+++ InteApp/src/com/inteapp/businessObject/Empleado.java
@@ -0,0 +1,11 @@
+package com.inteapp.businessObject;
+
+public class Empleado {
+	private String cuil;
+	private String nombre;
+	private String apellido;
+	private String direccion;
+	private String mail; 
+	private String nroCuenta;
+	private String tipoPersona;
+}
