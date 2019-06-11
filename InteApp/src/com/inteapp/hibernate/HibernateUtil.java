96955ff4aa7c6030e6f493c179a833e64fc2580c
matu_ <matu_@DESKTOP-AQ50H9J>
2019-06-11 00:45:21 -0300

    a

--- InteApp/src/com/inteapp/hibernate/HibernateUtil.java
+++ InteApp/src/com/inteapp/hibernate/HibernateUtil.java
@@ -3,7 +3,9 @@ package com.inteapp.hibernate;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 
+import com.inteapp.entities.CategoriaEntity;
 import com.inteapp.entities.ClienteEntity;
+import com.inteapp.entities.*;
 
 public class HibernateUtil {
 	private static final SessionFactory sessionFactory;
@@ -14,6 +16,15 @@ public class HibernateUtil {
         {
         	AnnotationConfiguration config = new AnnotationConfiguration();
         	config.addAnnotatedClass(ClienteEntity.class);
+        	config.addAnnotatedClass(CategoriaEntity.class);
+        	config.addAnnotatedClass(ConceptoEntity.class);
+        	config.addAnnotatedClass(EmpleadoEntity.class);
+        	config.addAnnotatedClass(EscalaEntity.class);
+        	config.addAnnotatedClass(FacturaEntity.class);
+        	config.addAnnotatedClass(RubroEntity.class);
+        	config.addAnnotatedClass(ItemRubroEntity.class);
+        	config.addAnnotatedClass(LiquidacionEntity.class);
+        	config.addAnnotatedClass(NovedadEntity.class);
             sessionFactory = config.buildSessionFactory();
         }
         catch (Throwable ex)
