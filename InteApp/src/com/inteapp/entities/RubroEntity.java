abe3e844e61fb68f062bf3fe2ccf5e3e108c8149
cristianbessone <cristianbessone@192.168.1.12>
2019-06-10 22:57:59 -0300

    RubroEntity

--- InteApp/src/com/inteapp/entities/RubroEntity.java
+++ InteApp/src/com/inteapp/entities/RubroEntity.java
@@ -1,8 +1,28 @@
 package com.inteapp.entities;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import com.inteapp.businessObject.ItemRubro;
+
+@Entity
+@Table (name="RUBROS")
 public class RubroEntity {
 
-	
+	@Column (name= "nombre")	
+	private String nombre;
+
+	@Column (name= "convenio")
+	private String convenio;
+
+	@OneToMany
+	@JoinColumn (name ="itemRubro") 
+	private List <ItemRubro> conceptos;
+
 	
 
 }
